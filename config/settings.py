34ad6c9b054363b33fac898b757f2c3368da8c3f
Rumatalive <nsanzamahoro250fabrice@gmail.com>
2026-09-16 11:26:05 -0700

    fix static root for render

--- config/settings.py
+++ config/settings.py
@@ -62,6 +62,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
+static_root = BASE_DIR / "staticfiles"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "/"
