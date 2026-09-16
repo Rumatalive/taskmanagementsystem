cc398f8fa11e6e2d39974ec7dbd9f5fef3cf8e85
Rumatalive <nsanzamahoro250fabrice@gmail.com>
2026-09-16 11:33:22 -0700

    fix static root caps

--- config/settings.py
+++ config/settings.py
@@ -60,9 +60,9 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
-static_root = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "/"
